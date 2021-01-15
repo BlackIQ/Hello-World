6dcef6b1cb337e90881b53dfd002ecda03d7e9c2
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-01-15 18:04:51 +0330

    Write Csharp file

---
+++ CSharp.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Hello-World
+{
+    class CSharp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World !");
+        }
+    }
+}
