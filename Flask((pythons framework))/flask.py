80737a11e9de9c7eafd04312d9f3b9e2d16636f8
Shahriar <shahriaarrr@gmail.com>
2021-01-21 09:38:27 +0330

    add flask framework

---
+++ Flask((pythons framework))/flask.py
@@ -0,0 +1,11 @@
+#install flask first.
+from flask import Flask
+
+app = Flask(__name__)
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello World"
+    #for run this file write on CMD:flask run
