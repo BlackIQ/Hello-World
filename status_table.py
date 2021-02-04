5ef86cebb736b57611c6fccfffa18b001e6118b6
parsa shahmaleki <parsampsh@gmail.com>
2021-02-04 17:11:33 +0330

    add a space between emoji and word

--- status_table.py
+++ status_table.py
@@ -73,11 +73,11 @@ def main(project_dir=None):
 
     # generate output from loaded list
     for item in real_list:
-        output += "| [ 🌐" + item['name'] + "](/" + item['name'] + ") |"
-        output += ('✅Done!' if item['readme'] else '[ℹ️Edit it!](/' + item['name'] + '/README.md)') + ' | '
-        output += ('✅Done!' if item['books'] else '[ℹ️Add one!](/' + item['name'] + '/books.md)') + ' | '
-        output += ('✅Done!' if item['courses'] else '[ℹ️Add one!](/' + item['name'] + '/courses.md)') + ' | '
-        output += ('✅Done!' if item['resources'] else '[ℹ️Add one!](/' + item['name'] + '/resources.md)') + ' | '
+        output += "| [ 🌐 " + item['name'] + "](/" + item['name'] + ") |"
+        output += ('✅ Done!' if item['readme'] else '[ℹ️ Edit it!](/' + item['name'] + '/README.md)') + ' | '
+        output += ('✅ Done!' if item['books'] else '[ℹ️ Add one!](/' + item['name'] + '/books.md)') + ' | '
+        output += ('✅ Done!' if item['courses'] else '[ℹ️ Add one!](/' + item['name'] + '/courses.md)') + ' | '
+        output += ('✅ Done!' if item['resources'] else '[ℹ️ Add one!](/' + item['name'] + '/resources.md)') + ' | '
         output += '\n'
 
     # write generated table
