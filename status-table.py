b04f70a956f96e13d363ddb246501ae100ae4ac8
parsa shahmaleki <parsampsh@gmail.com>
2021-02-04 15:06:14 +0330

    Add status table

---
+++ status-table.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import os
+
+output = """
+| Language | Readme | Books | Courses | Resources |
+|----------|--------|-------|---------|-----------|
+"""
+
+items = os.listdir()
+items.sort()
+
+for item in items:
+    if item[0] != '.' and os.path.isdir(item):
+        readme = open(item + '/README.md', 'r')
+        readme_completed = '\n' in readme.read().strip()
+        readme.close()
+
+        books = open(item + '/books.md', 'r')
+        books_completed = books.read().strip().split('\n')
+        books_completed = len([line for line in books_completed if line.strip().startswith('- [')]) >= 2
+        books.close()
+
+        courses = open(item + '/courses.md', 'r')
+        courses_completed = courses.read().strip().split('\n')
+        courses_completed = len([line for line in courses_completed if line.strip().startswith('- [')]) >= 2
+        courses.close()
+
+        resources = open(item + '/resources.md', 'r')
+        resources_completed = resources.read().strip().split('\n')
+        resources_completed = len([line for line in resources_completed if line.strip().startswith('- [')]) >= 2
+        resources.close()
+
+
+        output += "|"+item+"|"
+        output += '[' + ('x' if readme_completed else ' ') + ']|'
+        output += '[' + ('x' if books_completed else ' ') + ']|'
+        output += '[' + ('x' if courses_completed else ' ') + ']|'
+        output += '[' + ('x' if resources_completed else ' ') + ']'
+
+        output += '\n'
+
+
+print(output)
+
