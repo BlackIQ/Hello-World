3f4d337535bc0e32b293b5e05614e721a805be29
Masoud Nickparvar <masoudnick85@gmail.com>
2021-05-28 11:59:47 +0430

    Update Hello-world.js

--- Jquery/examples/Hello-world.js
+++ Jquery/examples/Hello-world.js
@@ -2,8 +2,4 @@ $(document).ready(function(){
   $("body").append("Hello world!");
 });
 
-// There are several ways to start using jQuery on your web site. You can:
-
-// - Download the jQuery library from jQuery.com
-// - Include jQuery from a CDN, like Google
 
